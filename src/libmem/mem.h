26a8dbdcef18a29fcbd8809a51c96e2b90babbb9
Ammar Ratnani <ammrat13@gmail.com>
2021-03-15 22:19:49 -0400

    Minor fixes

--- src/libmem/mem.h
+++ src/libmem/mem.h
@@ -24,12 +24,6 @@ __attribute__((target("thumb"))) void *memset(void *s, int c, size_t n);
 __attribute__((target("thumb"))) void *memcpy(void *dest, const void *src, size_t n);
 __attribute__((target("thumb"))) void *memmove(void *dest, const void *src, size_t n);
 
-// From `aeabi_mem_movement.c`
-__attribute__((target("thumb"))) void __aeabi_memcpy(void *dest, const void *src, size_t n);
-__attribute__((target("thumb"))) void __aeabi_memmove(void *dest, const void *src, size_t n);
-__attribute__((target("thumb"))) void __aeabi_memset(void *s, size_t n, int c);
-__attribute__((target("thumb"))) void __aeabi_memclr(void *s, size_t n, int c);
-
 
 #ifdef __cplusplus
 } // extern "C"
