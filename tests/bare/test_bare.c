4002b5ef56d9de2e470c40ac8e1fa0041677af68
Ammar Ratnani <ammrat13@gmail.com>
2021-03-19 12:26:33 -0400

    Added INIT_FILE

--- tests/bare/test_bare.c
+++ tests/bare/test_bare.c
@@ -7,19 +7,26 @@
 
 #include "runner.h"
 
-START_TEST(write_name) {
-    printf("# bare\n");
-} END_TEST
+INIT_FILE(bare);
 
+
+// A test that always passes
 START_TEST(do_test) {
     ck_assert_int_eq(0,0);
 } END_TEST
 
+
+// Return the suite to run
 Suite *suite_bare(void) {
+    // Setup
     Suite *ret = suite_create("bare");
     TCase *cas = tcase_create("do_test");
-    tcase_add_test(cas, write_name);
+    tcase_add_unchecked_fixture(cas, bare_print, NULL);
+
+    // Add tests
     tcase_add_test(cas, do_test);
+
+    // Return
     suite_add_tcase(ret, cas);
     return ret;
 }
--- tests/runner.h
+++ tests/runner.h
@@ -4,7 +4,8 @@
 
     A header file for the main test runner. It has forward declarations of all
     the functions returning suites, as well as a null-terminated list of all of
-    them for `main` to iterate over. It also includes Check and stdio.
+    them for `main` to iterate over. It also includes Check and stdio. Finally,
+    it has convinience macros for writing tests.
 */
 
 #ifndef RUNNER_H
@@ -18,6 +19,18 @@ extern "C" {
 // Include libraries
 #include <stdio.h>
 #include <check.h>
+
+// Defines name##_printed which holds whether the current file's name has been
+//  printed as a comment for TAP
+// Also defines name##_print to print the TAP comment if it hasn't already
+#define INIT_FILE(name) \
+    static int name##_printed = 0; \
+    static void name##_print(void) { \
+        if(!name##_printed) { \
+            printf("# %s\n", #name); \
+        } \
+        name##_printed = 1; \
+    } \
 
 
 // A null-terminated list of all the functions
