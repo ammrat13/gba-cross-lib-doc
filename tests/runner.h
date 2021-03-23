8d6f565d6ec1aca1b6e964c7016ba29662094aca
Ammar Ratnani <ammrat13@gmail.com>
2021-03-22 21:59:18 -0400

    Migrate to CMockA

--- tests/runner.h
+++ tests/runner.h
@@ -2,10 +2,10 @@
     runner.h
     By: Ammar Ratnani
 
-    A header file for the main test runner. It has forward declarations of all
-    the functions returning suites, as well as a null-terminated list of all of
-    them for `main` to iterate over. It also includes Check and stdio. Finally,
-    it has convinience macros for writing tests.
+    A header file for the main test runner. It has forward declarations for all
+    the group test functions to call for unit testing, as well as a declaration
+    for a null-terminated array of all of them. This header also includes cmocka
+    (and its required headers) and stdio.
 */
 
 #ifndef RUNNER_H
@@ -18,27 +18,22 @@ extern "C" {
 
 // Include libraries
 #include <stdio.h>
-#include <check.h>
-
-// Defines name##_printed which holds whether the current file's name has been
-//  printed as a comment for TAP
-// Also defines name##_print to print the TAP comment if it hasn't already
-#define INIT_FILE(name) \
-    static int name##_printed = 0; \
-    static void name##_print(void) { \
-        if(!name##_printed) { \
-            printf("# %s\n", #name); \
-        } \
-        name##_printed = 1; \
-    } \
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
 
 
-// A null-terminated list of all the functions
+// Functions that run `cmocka_run_group_tests`
+// Should return the result of that call - the number of failed tests
+typedef int group_test_run_t(void);
+
+// Forward declarations of all the group test functions
+group_test_run_t run_bare;
+
+// A null-terminated list of all those functions
 // Defined in `runner.c`
-extern Suite *(*SUITE_FUNCS[])(void);
-
-// Forward declarations of all the test suite functions
-Suite *suite_bare(void);
+extern group_test_run_t *GROUP_TEST_RUNS[];
 
 
 #ifdef __cplusplus
