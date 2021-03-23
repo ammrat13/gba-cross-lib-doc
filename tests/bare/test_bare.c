1478e3893af0045744c8b6b7edea004ab24f7a1d
Ammar Ratnani <ammrat13@gmail.com>
2021-03-23 14:47:32 -0400

    Finsihed core init and started writing display

--- tests/bare/test_bare.c
+++ tests/bare/test_bare.c
@@ -5,7 +5,7 @@
     Check that `game_bare.c` gives the right output to the screen. Should be
     mostly black save for three pixels that are red, green, and blue.
 
-    For this file, the `state` is an `struct mCore**`
+    For this file, the `state` is an `state_t**`
 */
 
 #include "runner.h"
@@ -14,50 +14,75 @@
 #include <mgba/core/core.h>
 
 
+// Define the state type
+typedef struct state_t {
+    // The GBA core
+    struct mCore* core;
+    // The video buffer and specifications
+    unsigned int videoWidth;
+    unsigned int videoHeight;
+    color_t *videoBuffer;
+} state_t;
+
+
 // Setup the core
 static int setup_core(void **state) {
 
+    // Create the state structure
+    state_t *s = malloc(sizeof(state_t));
+    if(s == NULL) {
+        fail_msg("Failed to allocate state");
+    }
+
     // Create the core
-    struct mCore *core = mCoreFind("bare/game_bare.to");
-    assert_non_null(core);
-    // Initialize
-    core->init(core);
+    s->core = mCoreFind("bare/game_bare.thex");
+    if(s->core == NULL) {
+        free(s);
+        fail_msg("Failed to allocate GBA core");
+    }
+    // Initialize it
+    s->core->init(s->core);
+
+    // Get video buffer dimensions
+    s->core->desiredVideoDimensions(s->core, &s->videoWidth, &s->videoHeight);
+    // Allocate the video buffer
+    s->videoBuffer = malloc(s->videoHeight * s->videoWidth * BYTES_PER_PIXEL);
+    if(s->videoBuffer == NULL) {
+        free(s->core);
+        free(s);
+        fail_msg("Failed to allocate video buffer");
+    }
+    // Initialize the video buffer
+    s->core->setVideoBuffer(s->core, s->videoBuffer, s->videoWidth);
 
     // Configure the core
-    mCoreConfigInit(&core->config, NULL);
-    mCoreConfigSetValue(&core->config, "idleOptimization", "ignore");
+    mCoreConfigInit(&s->core->config, NULL);
+    mCoreConfigSetValue(&s->core->config, "idleOptimization", "ignore");
 
     // Actually load the file
-    mCoreLoadFile(core, "bare/game_bare.to");
+    mCoreLoadFile(s->core, "bare/game_bare.thex");
 
-    // Return
-    *state = core;
+    // Return the data
+    *state = s;
     return 0;
 }
 
 // Check that the display is correct
 static void test_display(void **state) {
 
-    // Get the core
-    struct mCore *core = *state;
+    // Get the state
+    state_t *s = *state;
 
-    // Get video buffer dimensions
-    unsigned int videoHeight, videoWidth;
-    core->desiredVideoDimensions(core, &videoWidth, &videoHeight);
-    // Initialize the video buffer
-    color_t videoBuffer[videoHeight][videoWidth];
-    core->setVideoBuffer(core, (color_t*) videoBuffer, videoWidth);
-
+    // Reset the core
+    s->core->reset(s->core);
     // Step it for some time
     for(int i = 0; i < 1000; i++) {
-        core->step(core);
+        s->core->step(s->core);
     }
 
     // Check that the video buffer has the right contents
-    for(unsigned int r = 0; r < videoHeight; r++) {
-        for(unsigned int c = 0; c < videoWidth; c++) {
-            assert_int_equal(videoBuffer[r][c], 0);
-        }
+    for(unsigned int i = 0; i < s->videoHeight * s->videoWidth; i++) {
+        assert_int_equal(s->videoBuffer[i], 0);
     }
 }
 
